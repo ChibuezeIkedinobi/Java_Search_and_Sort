987a5923d1500e24cfb0acb3ff63fa5633541a5e
Ikedinobi <Ikdinobi@gmail.com>
2023-05-04 23:32:43 +0100

    Search and Sort

--- src/binarySearch/binary.java
+++ src/binarySearch/binary.java
@@ -4,7 +4,6 @@ public class binary {
 
     public static void main(String[] args) {
 
-//        BinarySearch ob = new BinarySearch();
         int arr[] = { 2, 3, 4, 10, 40 };
         int n = arr.length;
         int x = 10;
